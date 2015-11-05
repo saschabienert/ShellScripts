bdf7c229d6d06af0062c1bbed1aa924834f7038d
Sascha Bienert <sascha@twiddlefactor.de>
2015-11-05 12:31:39 +0100

    checks for number of parameters

--- fyberfeature.sh
+++ fyberfeature.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 # checkout fyber feature
 
-UPSTREAM="$1"
-BRANCH="$2"
-MYREMOTE="$3"
-MYBRANCH="$4"
+if [ $# -eq 4 ]; then
 
-git fetch $MYREMOTE && \
-git fetch $UPSTREAM && \
-git checkout -b $BRANCH $MYREMOTE/$BRANCH || \
-git checkout $BRANCH && \
-git merge $UPSTREAM/$BRANCH && \
-git checkout -b $MYBRANCH && \
-# git push -u $MYREMOTE $MYBRANCH && \
-bundle install && \
-bundle exec pod install
+	UPSTREAM="$1"
+	BRANCH="$2"
+	MYREMOTE="$3"
+	MYBRANCH="$4"
+
+	git fetch $MYREMOTE && \
+	git fetch $UPSTREAM && \
+	git checkout -b $BRANCH $MYREMOTE/$BRANCH || \
+	git checkout $BRANCH && \
+	git merge $UPSTREAM/$BRANCH && \
+	git checkout -b $MYBRANCH && \
+	# git push -u $MYREMOTE $MYBRANCH && \
+	bundle install && \
+	bundle exec pod install
+
+else
+
+	echo "error:   missing parameter"
+	echo "usage:   fyberfeature.sh [upstream] [branch] [your_remote] [ticket_branch]"
+	echo "example: fyberfeature.sh upstream develop-minor sascha IOS-1000"
+
+fi
